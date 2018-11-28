33b379214fad4ee9f9457ce5f235e28150f8db4b
Nikita Shkorodenok <nikita.shkorodenok@effective-soft.com>
2018-11-28 13:22:52 +0300

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,9 +14,22 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+	socket.emit('newMessage', {
+		from: 'Admin',
+		text: 'Welcome to the chat app!',
+		createdAt: new Date().getTime()
+	});
+
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'Admin was joined to the chat app',
+		createdAt: new Date().getTime()
+	})
+
 	socket.on('createMessage', (message) => {
 		console.log('createMessage', message);
 		io.emit('newMessage', { ...message, createdAt: new Date().getTime() });
+		//socket.broadcast.emit('newMessage', { ...message, createdAt: new Date().getTime() });
 	});
 
 
